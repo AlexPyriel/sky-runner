3ac4e8410ee0bd0018e165781aa773e8ec97c16c
AlexPyriel <alexpyriel@yandex.ru>
2023-07-24 19:58:09 +0300

    update level generation

--- Assets/Source/Player/PlayerController.cs
+++ Assets/Source/Player/PlayerController.cs
@@ -1,16 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private PlayerControls _playerControls;
+
+    private void Awake()
+    {
+        _playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+        _playerControls.Player.Jump.performed += Jump;
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Player.Jump.performed -= Jump;
+        _playerControls.Disable();
+    }
+
     private void Start()
     {
-        
+
     }
 
     private void Update()
     {
-        
+
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump");
     }
 }
