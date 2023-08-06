36cb700cb8dafdcd16e81dc72129689440f43169
AlexPyriel <alexpyriel@yandex.ru>
2023-08-07 01:46:39 +0300

    grappling rope variables renamed

--- Assets/Source/Player/Grappling Gun/GrapplingRope.cs
+++ Assets/Source/Player/Grappling Gun/GrapplingRope.cs
@@ -13,15 +13,15 @@ public class GrapplingRope : MonoBehaviour
     [SerializeField] private float _velocity = 15; // velocity of the animation
     [SerializeField] private float _waveCount = 3; // how many waves are being simulated
     [SerializeField] private float _waveHeight = 1;
-    [SerializeField] private AnimationCurve affectCurve;
+    [SerializeField] private AnimationCurve _affectCurve;
 
-    private Spring spring; // a custom script that returns the values needed for the animation
-    private Vector3 currentGrapplePosition;
+    private Spring _spring; // a custom script that returns the values needed for the animation
+    private Vector3 _currentGrapplePosition;
 
     private void Awake()
     {
-        spring = new Spring();
-        spring.SetTarget(0);
+        _spring = new Spring();
+        _spring.SetTarget(0);
     }
 
     private void LateUpdate()
@@ -34,10 +34,10 @@ public class GrapplingRope : MonoBehaviour
         // if not grappling, don't draw rope
         if (!_grapplingGun.IsAttached)
         {
-            currentGrapplePosition = _grapplingGun.GunTip.position;
+            _currentGrapplePosition = _grapplingGun.GunTip.position;
 
             // reset the simulation
-            spring.Reset();
+            _spring.Reset();
 
             // reset the positionCount of the lineRenderer
             if (_lineRenderer.positionCount > 0)
@@ -49,16 +49,16 @@ public class GrapplingRope : MonoBehaviour
         if (_lineRenderer.positionCount == 0)
         {
             // set the start velocity of the simulation
-            spring.SetVelocity(_velocity);
+            _spring.SetVelocity(_velocity);
 
             // set the positionCount of the lineRenderer depending on the quality of the rope
             _lineRenderer.positionCount = _quality + 1;
         }
 
         // set the spring simulation
-        spring.SetDamper(_damper);
-        spring.SetStrength(_strength);
-        spring.Update(Time.deltaTime);
+        _spring.SetDamper(_damper);
+        _spring.SetStrength(_strength);
+        _spring.Update(Time.deltaTime);
 
         Vector3 grapplePoint = _grapplingGun.SwingPoint;
         Vector3 gunTipPosition = _grapplingGun.GunTip.position;
@@ -67,17 +67,17 @@ public class GrapplingRope : MonoBehaviour
         Vector3 up = Quaternion.LookRotation((grapplePoint - gunTipPosition).normalized) * Vector3.up;
 
         // lerp the currentGrapplePositin towards the grapplePoint
-        currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, grapplePoint, Time.deltaTime * 8f);
+        _currentGrapplePosition = Vector3.Lerp(_currentGrapplePosition, grapplePoint, Time.deltaTime * 8f);
 
         // loop through all segments of the rope and animate them
         for (int i = 0; i < _quality + 1; i++)
         {
             float delta = i / (float)_quality;
             // calculate the offset of the current rope segment
-            Vector3 offset = up * _waveHeight * Mathf.Sin(delta * _waveCount * Mathf.PI) * spring.Value * affectCurve.Evaluate(delta);
+            Vector3 offset = up * _waveHeight * Mathf.Sin(delta * _waveCount * Mathf.PI) * _spring.Value * _affectCurve.Evaluate(delta);
 
             // lerp the lineRenderer position towards the currentGrapplePosition + the offset you just calculated
-            _lineRenderer.SetPosition(i, Vector3.Lerp(gunTipPosition, currentGrapplePosition, delta) + offset);
+            _lineRenderer.SetPosition(i, Vector3.Lerp(gunTipPosition, _currentGrapplePosition, delta) + offset);
         }
     }
 }
