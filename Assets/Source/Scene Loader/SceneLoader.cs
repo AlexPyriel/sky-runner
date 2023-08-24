865767c10ff381c0e6978d0a0024ea0d2b417965
AlexPyriel <alexpyriel@yandex.ru>
2023-08-24 21:35:09 +0300

    minor UI updates

--- Assets/Source/Scene Loader/SceneLoader.cs
+++ Assets/Source/Scene Loader/SceneLoader.cs
@@ -6,15 +6,22 @@ public class SceneLoader : MonoBehaviour
     [SerializeField] private SceneLoaderAnimation _sceneLoaderAnimation;
     [SerializeField] private GameConfig _gameConfig;
 
+    private float _loadDelay = 1.7f;
+
     public void LoadGameScene()
     {
         _sceneLoaderAnimation.Reveal();
-        Game_Scene.Load(_gameConfig);
+        Invoke(nameof(LoadGame), _loadDelay);
     }
 
-    private void LoadTitleScene()
+    private void LoadStartcene()
     {
         _sceneLoaderAnimation.Reveal();
         Start_Scene.Load();
+    }
+
+    private void LoadGame()
+    {
+        Game_Scene.Load(_gameConfig);
     }
 }
