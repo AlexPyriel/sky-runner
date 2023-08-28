f22e786943fd17ae0a04ce298978ffadd8dcd4be
AlexPyriel <alexpyriel@yandex.ru>
2023-08-28 21:50:47 +0300

    added kinematic rb to sensor to separate  parent

--- Assets/Source/Player/Player.cs
+++ Assets/Source/Player/Player.cs
@@ -8,21 +8,21 @@ public class Player : MonoBehaviour
     public event Action Collected;
     public event Action ObstacleCollided;
 
-    // private void OnTriggerEnter(Collider other)
-    // {
-    //     Debug.Log($"Colleded {other.name}");
-    //     if (other.TryGetComponent<Coin>(out Coin coin))
-    //     {
-    //         Collected?.Invoke();
-    //         Destroy(other.gameObject);
-    //         other.gameObject.SetActive(false);
-    //     }
-    //     else if (other.TryGetComponent<Powerup>(out Powerup powerup))
-    //     {
-    //         ObstacleCollided?.Invoke();
-    //         powerup.Destroy();
-    //     }
-    // }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Spot trigger {other.name}");
+        // if (other.TryGetComponent<Coin>(out Coin coin))
+        // {
+        //     Collected?.Invoke();
+        //     Destroy(other.gameObject);
+        //     other.gameObject.SetActive(false);
+        // }
+        // else if (other.TryGetComponent<Powerup>(out Powerup powerup))
+        // {
+        //     ObstacleCollided?.Invoke();
+        //     powerup.Destroy();
+        // }
+    }
 
     private void OnCollisionEnter(Collision other)
     {
